85a006a6ffdf45e26ebcec478f8fbe3f437bfdb5
juliawlett <126482151+juliawlett@users.noreply.github.com>
2023-10-15 23:20:32 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Portfolio from "./pages/Portfolios";
 
 function App() {
   return (
     <div className="App">
-      <Route exact path="https://portfolio-sigma-blush-98.vercel.app/" element={<Portfolio />} />
+      <Route exact path="https://portfolio-3ipfuc3ss-juliawlett.vercel.app/" element={<Portfolio />} />
     </div>
   );
 }
