927a35e2c2329ad57b3151349d7d256df935ec2d
juliawlett <126482151+juliawlett@users.noreply.github.com>
2023-10-15 23:13:27 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Portfolio from "./pages/Portfolios";
 
 function App() {
   return (
     <div className="App">
-      <Route exact path="/" element={<Portfolio />} />
+      {<Portfolio />
     </div>
   );
 }
