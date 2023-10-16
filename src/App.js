e3edb80daf1bd23e642cac5f61960ce1e2bdf6f4
juliawlett <126482151+juliawlett@users.noreply.github.com>
2023-10-15 23:18:33 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Portfolio from "./pages/Portfolios";
 function App() {
   return (
     <div className="App">
-      <Portfolio />
+      <Route exact path="https://portfolio-sigma-blush-98.vercel.app/" element={<Portfolio />} />
     </div>
   );
 }
