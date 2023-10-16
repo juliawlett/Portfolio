5f680aace3d6a1cbf635c12cf1a9442b2112dce5
juliawlett <126482151+juliawlett@users.noreply.github.com>
2023-10-15 23:29:29 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Portfolio from "./pages/Portfolios";
 function App() {
   return (
     <div className="App">
-      <Route exact path="https://portfolio-juliawlett.vercel.app/" element={<Portfolio />} />
+      <Portfolio />
     </div>
   );
 }
